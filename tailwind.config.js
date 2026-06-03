f255f9ef753bf2e4ff42c75031066a129daf6d62
constellation-agent <constellation@noreply.local>
2026-06-02 22:35:50 -0400

    feat(CSTL-1): implement changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,7 +54,7 @@ export default {
       borderRadius: {
         DEFAULT: "0.125rem",
         lg: "0.25rem",
-        xl: "0.5rem",
+        xl: "0.75rem",
         full: "0.75rem"
       },
       spacing: {
