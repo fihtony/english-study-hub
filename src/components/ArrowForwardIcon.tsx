346331a54b1b94602339be943bf1fd3177add73d
constellation-agent <constellation@noreply.local>
2026-06-07 19:58:59 -0400

    feat(CSTL-2): implement changes

--- src/components/ArrowForwardIcon.tsx
+++ src/components/ArrowForwardIcon.tsx
@@ -1,13 +1,12 @@
 const ArrowForwardIcon = () => (
   <svg
-    className="material-symbols-outlined text-outline group-hover:text-secondary group-hover:translate-x-1 transition-all"
-    style={{ fontVariationSettings: "'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24" }}
+    className="text-outline group-hover:text-secondary group-hover:translate-x-1 transition-all"
     width="24"
     height="24"
     viewBox="0 0 24 24"
     aria-hidden="true"
   >
-    <path d="M124l-1.41 1.41L16.1711H4v2h12.17l-5.58 5.59L12 20l8-8z" />
+    <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" />
   </svg>
 )
 
